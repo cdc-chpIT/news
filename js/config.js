3f3051b7e0c756468e1fdadc12ea5d78017dbbd0
VuHPhuc <boycute2002@gmail.com>
2026-01-22 14:38:44 +0700

    Update API_BASE_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 const config = {
-    API_BASE_URL: 'https://d42735dece47.ngrok-free.app/api/v1'
+    API_BASE_URL: 'https://8803b51279d3.ngrok-free.app  /api/v1'
 };
+//(Invoke-RestMethod -Uri "http://localhost:4040/api/tunnels").tunnels[0].public_url
