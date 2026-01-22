15e9d5b94d5fa4d653a2ab2834d77e5fafcf3e78
VuHPhuc <boycute2002@gmail.com>
2026-01-22 14:39:01 +0700

    Fix: correct API_BASE_URL formatting in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const config = {
-    API_BASE_URL: 'https://8803b51279d3.ngrok-free.app  /api/v1'
+    API_BASE_URL: 'https://8803b51279d3.ngrok-free.app/api/v1'
 };
 //(Invoke-RestMethod -Uri "http://localhost:4040/api/tunnels").tunnels[0].public_url
