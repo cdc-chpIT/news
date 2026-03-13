65f481c94431afcafb14c1d91ec47c281535c84e
VuHPhuc <boycute2002@gmail.com>
2026-03-13 15:43:16 +0700

    Update API_BASE_URL in config.js to new ngrok endpoint

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const config = {
-    API_BASE_URL: 'https://dd57-118-70-187-46.ngrok-free.app/api/v1'
+    API_BASE_URL: 'https://d8af-118-70-187-46.ngrok-free.app/api/v1'
 };
 //(Invoke-RestMethod -Uri "http://localhost:4040/api/tunnels").tunnels[0].public_url
