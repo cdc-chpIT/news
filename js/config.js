c901bf009c1a44db70eeb30b04a57820f779cca3
VuHPhuc <boycute2002@gmail.com>
2026-06-17 09:42:52 +0700

    feat: add configuration file with API base URL endpoint

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const config = {
-    API_BASE_URL: 'https://0f43-1-55-219-55.ngrok-free.app/api/v1'
+    API_BASE_URL: 'https://c539-1-54-8-24.ngrok-free.app/api/v1'
 };
 //(Invoke-RestMethod -Uri "http://localhost:4040/api/tunnels").tunnels[0].public_url
