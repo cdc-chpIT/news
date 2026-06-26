950888c9e30af8bb325be2881ff8e4be853bbaa4
VuHPhuc <boycute2002@gmail.com>
2026-06-26 14:53:52 +0700

    update config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const config = {
-    API_BASE_URL: 'https://c539-1-54-8-24.ngrok-free.app/api/v1'
+    API_BASE_URL: 'https://c799-1-54-8-24.ngrok-free.app/api/v1'
 };
 //(Invoke-RestMethod -Uri "http://localhost:4040/api/tunnels").tunnels[0].public_url
